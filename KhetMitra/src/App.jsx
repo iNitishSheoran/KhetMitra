026fa2b0d5d17782fee536dc3d7614c0365d05d1
Sonal Tyagi <sonaltyagi.biz@gmail.com>
2025-09-06 15:48:51 +0530

    Edited App.jsx

--- KhetMitra/src/App.jsx
+++ KhetMitra/src/App.jsx
@@ -1,10 +1,4 @@
 import './App.css'
-<<<<<<< HEAD
-import Navbar from './components/Navbar'
-import Body from './pages/Body'
-import About from './pages/About'
-function App() {
-=======
 import { Routes, Route } from "react-router-dom";
 import Navbar from './components/Navbar';
 import Body from './pages/Body';
@@ -12,16 +6,10 @@ import Body from './pages/Body';
 import HelpForm from './pages/HelpForm';
 // import MyRequests from './pages/MyRequests';
 // import AdminHelpPage from './pages/AdminHelpPage';
->>>>>>> c7e4f2288bc34cf48bcf37e2160edb6fe0b22fa1
 
 function App() {
   return (
     <div>
-<<<<<<< HEAD
-      <Navbar/>
-      <Body/>
-      <About/>
-=======
       <Navbar />
       <Routes>
         <Route path="/" element={<Body />} />
@@ -29,7 +17,6 @@ function App() {
         {/* <Route path="/my-requests" element={<MyRequests />} />
         <Route path="/admin/help" element={<AdminHelpPage />} /> */}
       </Routes>
->>>>>>> c7e4f2288bc34cf48bcf37e2160edb6fe0b22fa1
     </div>
   );
 }
