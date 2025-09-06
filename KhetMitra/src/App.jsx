1eea595ee632c58f2b5c39f2d50b5f8a65bfe8c8
iNitishSheoran <121354290+iNitishSheoran@users.noreply.github.com>
2025-09-06 14:56:52 +0530

    About.jsx done

--- KhetMitra/src/App.jsx
+++ KhetMitra/src/App.jsx
@@ -1,13 +1,14 @@
 import './App.css'
 import Navbar from './components/Navbar'
 import Body from './pages/Body'
-
+import About from './pages/About'
 function App() {
 
   return (
     <div>
       <Navbar/>
       <Body/>
+      <About/>
     </div>
   )
 }
