9a7614dfc49a1a007e45ddf33c6fefe02deb3c12
Sonal Tyagi <sonaltyagi.biz@gmail.com>
2025-09-10 23:35:48 +0530

    Added kmstudio to Navbar

--- KhetMitra/src/components/Navbar.jsx
+++ KhetMitra/src/components/Navbar.jsx
@@ -58,6 +58,7 @@ export default function Navbar() {
         <Link to="/about" className="hover:text-[#4ADE80] hover:scale-105 transition-all duration-200">About Us</Link>
         <Link to="/help" className="hover:text-[#4ADE80] hover:scale-105 transition-all duration-200">Help</Link>
         <Link to="/daam" className="hover:text-[#4ADE80] hover:scale-105 transition-all duration-200">Daam</Link>
+        <Link to="/kmstudio" className="hover:text-[#4ADE80] hover:scale-105 transition-all duration-200">KM-Studio</Link>
       </div>
 
       {/* Profile */}
