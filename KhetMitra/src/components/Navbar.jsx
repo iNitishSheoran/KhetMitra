e1908d5b5c80eb3de96f18b28ec1d00a1599fed4
iNitishSheoran <121354290+iNitishSheoran@users.noreply.github.com>
2025-09-12 23:58:46 +0530

    bazaarbhav

--- KhetMitra/src/components/Navbar.jsx
+++ KhetMitra/src/components/Navbar.jsx
@@ -57,7 +57,7 @@ export default function Navbar() {
         <Link to="/cropData" className="hover:text-[#4ADE80] hover:scale-105 transition-all duration-200">Crop Data</Link>
         <Link to="/about" className="hover:text-[#4ADE80] hover:scale-105 transition-all duration-200">About Us</Link>
         <Link to="/help" className="hover:text-[#4ADE80] hover:scale-105 transition-all duration-200">Help</Link>
-        <Link to="/daam" className="hover:text-[#4ADE80] hover:scale-105 transition-all duration-200">Daam</Link>
+        <Link to="/daam" className="hover:text-[#4ADE80] hover:scale-105 transition-all duration-200">BazaarBhav</Link>
         <Link to="/kmstudio" className="hover:text-[#FACC15] hover:scale-110 text-cyan-200 transition-all duration-300">KM-Studio</Link>
       </div>
 
